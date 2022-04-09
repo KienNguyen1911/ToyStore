8e67ef49b42019aa5c9694bad3468579a86a4635
KienNguyen1911 <kienndbhaf200045@fpt.edu.vn>
2022-04-09 10:36:36 +0700

    change config/app.php

--- config/app.php
+++ config/app.php
@@ -121,7 +121,10 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    // 'key' => env('APP_KEY'),
+
+    // 'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
